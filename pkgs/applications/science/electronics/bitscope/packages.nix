583c1935dbbfbf4adde3b512708b8cfe6c3d87d9
David Asabina <vid@bina.me>
2017-12-28 18:44:49 +0100

    bitscope: refactored, re-ordered set members

--- pkgs/applications/science/electronics/bitscope/packages.nix
+++ pkgs/applications/science/electronics/bitscope/packages.nix
@@ -52,6 +52,69 @@ let
     runScript = "${pkg.outPath}/bin/${attrs.toolName}";
   };
 in {
+  chart = let
+    name = "${toolName}_${version}";
+    toolName = "bitscope-chart";
+    version = "2.0.FK22M";
+  in mkBitscope {
+    inherit name toolName version;
+
+    meta = {
+      description = "Multi-channel waveform data acquisition and chart recording application";
+      homepage = "http://bitscope.com/software/chart/";
+    };
+
+    src = if stdenv.system == "x86_64-linux" then fetchurl {
+      url = "http://bitscope.com/download/files/${name}_amd64.deb";
+      sha256 = "08mc82pjamyyyhh15sagsv0sc7yx5v5n54bg60fpj7v41wdwrzxw";
+    } else if stdenv.system == "i686-linux" then fetchurl {
+      url = "http://bitscope.com/download/files/${name}_i386.deb";
+      sha256 = "08p6psh8g6pjp9gpdksb8y6nn9n4hv1j450v1nfhy25h2n8j0f03";
+    } else throw "no install instructions for ${stdenv.system}";
+  };
+
+  console = let
+    name = "${toolName}_${version}";
+    toolName = "bitscope-console";
+    version = "1.0.FK29A";
+  in mkBitscope {
+    # NOTE: this is meant as a demo by BitScope
+    inherit name toolName version;
+
+    meta = {
+      description = "Demonstrative communications program designed to make it easy to talk to any model BitScope";
+    };
+
+    src = if stdenv.system == "x86_64-linux" then fetchurl {
+      url = "http://bitscope.com/download/files/${name}_amd64.deb";
+      sha256 = "00b4gxwz7w6pmfrcz14326b24kl44hp0gzzqcqxwi5vws3f0y49d";
+    } else if stdenv.system == "i686-linux" then fetchurl {
+      url = "http://bitscope.com/download/files/${name}_i386.deb";
+      sha256 = "0llcvbgmwhl3dybv1c4sxxsgc4xl21rxk6bdva5a11w4w9sx6c7s";
+    } else throw "no install instructions for ${stdenv.system}";
+  };
+
+  display = let
+    name = "${toolName}_${version}";
+    toolName = "bitscope-display";
+    version = "1.0.EC17A";
+  in mkBitscope {
+    inherit name toolName version;
+
+    meta = {
+      description = "Display diagnostic application for BitScope";
+      homepage = "http://bitscope.com/software/display/";
+    };
+
+    src = if stdenv.system == "x86_64-linux" then fetchurl {
+      url = "http://bitscope.com/download/files/${name}_amd64.deb";
+      sha256 = "05xr5mnka1v3ibcasg74kmj6nlv1nmn3lca1wv77whkq85cmz0s1";
+    } else if stdenv.system == "i686-linux" then fetchurl {
+      url = "http://bitscope.com/download/files/${name}_i386.deb";
+      sha256 = "13csap7gmc1q19rd6qhd0z64bqafk03zfalnsrz2gxa2db5m5wlk";
+    } else throw "no install instructions for ${stdenv.system}";
+  };
+
   dso = let
     name = "${toolName}_${version}";
     toolName = "bitscope-dso";
@@ -115,36 +178,18 @@ in {
     } else throw "no install instructions for ${stdenv.system}";
   };
 
-  chart = let
-    name = "${toolName}_${version}";
-    toolName = "bitscope-chart";
-    version = "2.0.FK22M";
-  in mkBitscope {
-    inherit name toolName version;
-
-    meta = {
-      description = "Multi-channel waveform data acquisition and chart recording application";
-      homepage = "http://bitscope.com/software/chart/";
-    };
-
-    src = if stdenv.system == "x86_64-linux" then fetchurl {
-      url = "http://bitscope.com/download/files/${name}_amd64.deb";
-      sha256 = "08mc82pjamyyyhh15sagsv0sc7yx5v5n54bg60fpj7v41wdwrzxw";
-    } else if stdenv.system == "i686-linux" then fetchurl {
-      url = "http://bitscope.com/download/files/${name}_i386.deb";
-      sha256 = "08p6psh8g6pjp9gpdksb8y6nn9n4hv1j450v1nfhy25h2n8j0f03";
-    } else throw "no install instructions for ${stdenv.system}";
-  };
-
-  proto = mkBitscope rec {
-    # note: clicking on logo produces error
-    # TApplication.HandleException Executable not found: "http://bitscope.com/blog/DK/?p=DK15A"
+  proto = let
     name = "${toolName}_${version}";
     toolName = "bitscope-proto";
     version = "0.9.FG13B";
+  in mkBitscope rec {
+    inherit name toolName version;
+    # NOTE: this is meant as a demo by BitScope
+    # NOTE: clicking on logo produces error
+    # TApplication.HandleException Executable not found: "http://bitscope.com/blog/DK/?p=DK15A"
 
     meta = {
-      description = "Prototype oscilloscope built using the BitScope Library";
+      description = "Demonstrative prototype oscilloscope built using the BitScope Library";
       homepage = "http://bitscope.com/blog/DK/?p=DK15A";
     };
 
@@ -157,53 +202,17 @@ in {
     } else throw "no install instructions for ${stdenv.system}";
   };
 
-  console = let
-    # https://trello.com/c/sgH6tDdy/58-fb28a-bitscope-console
-    name = "${toolName}_${version}";
-    toolName = "bitscope-console";
-    version = "1.0.FK29A";
-  in mkBitscope {
-    inherit name toolName version;
-
-    meta.description = "Communications program designed to make it easy to talk to any model BitScope";
-    meta.longDescription = "BitScope Console is a communications program designed to make it easy to talk to any model BitScope via any supported communications link (serial, USB, LAN, Internet etc). Use it to interrogate the BitScope Virtual Machine to help write your own software for BitScope. It uses BitScope Link Library to facilitate communication. Note: you don't need the program to talk to most BitScopes, a simple terminal program will do but Console makes connection easier, uses the probe file specification and supports downstream communications to connected port adapters.";
-
-    src = if stdenv.system == "x86_64-linux" then fetchurl {
-      url = "http://bitscope.com/download/files/${name}_amd64.deb";
-      sha256 = "00b4gxwz7w6pmfrcz14326b24kl44hp0gzzqcqxwi5vws3f0y49d";
-    } else if stdenv.system == "i686-linux" then fetchurl {
-      url = "http://bitscope.com/download/files/${name}_i386.deb";
-      sha256 = "0llcvbgmwhl3dybv1c4sxxsgc4xl21rxk6bdva5a11w4w9sx6c7s";
-    } else throw "no install instructions for ${stdenv.system}";
-  };
-
-  display = let
-    name = "${toolName}_${version}";
-    toolName = "bitscope-display";
-    version = "1.0.EC17A";
-  in mkBitscope {
-    inherit name toolName version;
-
-    meta = {
-      description = "Display diagnostic application for BitScope";
-      homepage = "http://bitscope.com/software/display/";
-    };
-
-    src = if stdenv.system == "x86_64-linux" then fetchurl {
-      url = "http://bitscope.com/download/files/${name}_amd64.deb";
-      sha256 = "05xr5mnka1v3ibcasg74kmj6nlv1nmn3lca1wv77whkq85cmz0s1";
-    } else if stdenv.system == "i686-linux" then fetchurl {
-      url = "http://bitscope.com/download/files/${name}_i386.deb";
-      sha256 = "13csap7gmc1q19rd6qhd0z64bqafk03zfalnsrz2gxa2db5m5wlk";
-    } else throw "no install instructions for ${stdenv.system}";
-  };
-
   server = let
     name = "${toolName}_${version}";
     toolName = "bitscope-server";
     version = "1.0.FK26A";
   in mkBitscope {
     inherit name toolName version;
+
+    meta = {
+      description = "Remote access server solution for any BitScope";
+      homepage = "http://bitscope.com/software/server/";
+    };
 
     src = if stdenv.system == "x86_64-linux" then fetchurl {
       url = "http://bitscope.com/download/files/${name}_amd64.deb";
