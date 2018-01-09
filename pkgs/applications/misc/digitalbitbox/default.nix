af067e12741b4fa2b3b3060225f3f6538ddb4d0b
David Asabina <vid@bina.me>
2018-01-09 21:11:13 +0100

    fhs-ed and wrapped

--- pkgs/applications/misc/digitalbitbox/default.nix
+++ pkgs/applications/misc/digitalbitbox/default.nix
@@ -12,84 +12,106 @@
   libudev,
   libusb,
   libqrencode,
+  makeWrapper,
   pkgconfig,
   qt56,
   stdenv,
-  tree
+  tree,
+  writeScriptBin
 }:
 
-stdenv.mkDerivation rec {
-  name = "dbb-app";
+let
+  fhsEnv = buildFHSUserEnv { name = "dbb-fhs"; };
+  base = stdenv.mkDerivation rec {
+    name = "dbb-app";
 
-  src = fetchFromGitHub {
-    owner = "digitalbitbox";
-    repo = "dbb-app";
-    rev = "v${version}";
-    sha256 = "1r77fvqrlaryzij5dfbnigzhvg1d12g96qb2gp8dy3xph1j0k3s1";
+    src = fetchFromGitHub {
+      owner = "digitalbitbox";
+      repo = "dbb-app";
+      rev = "v${version}";
+      sha256 = "1r77fvqrlaryzij5dfbnigzhvg1d12g96qb2gp8dy3xph1j0k3s1";
+    };
+
+    version = "2.2.2";
+
+    #nativeBuildInputs = gccPkgs;
+
+    buildInputs = [
+      curl
+      libevent
+      libtool
+      libudev
+      libusb
+      libqrencode
+      pkgconfig
+    ] ++ gccPkgs ++ utilPkgs ++ qtPkgs;
+
+    qtPkgs = [
+      qt56.full
+    ];
+
+    gccPkgs = [
+      autoconf
+      automake
+      gcc49
+      #makeWrapper
+    ];
+
+    utilPkgs = [
+      #file
+      git
+      #less
+      #strace
+      tree
+      #which
+    ];
+
+    QTDIR="${qt56.qtbase.dev}";
+
+    MOC="${qt56.qtbase.dev}/bin/moc";
+    UIC="${qt56.qtbase.dev}/bin/uic";
+    RCC="${qt56.qtbase.dev}/bin/rcc";
+    LRELEASE="${qt56.qttools.dev}/bin/lrelease";
+    LUPDATE="${qt56.qttools.dev}/bin/lupdate";
+
+    preConfigure = "./autogen.sh";
+
+    configureFlags = [
+      "--enable-debug"
+      "--enable-libusb"
+      "--with-qt-bindir=${qt56.qtbase.dev}/bin:${qt56.qttools.dev}/bin"
+      #"--prefix=`realpath out`/tmp"
+    ];
+
+    hardeningDisable = [
+      "all"
+      #"format"
+    ];
+
+  #  writeTextFile {
+  #    name = "dbb-cli";
+  #    destination = "/bin/dbb-cli";
+  #    executable = true;
+  #    text = ''
+  #    '';
+  #  };
+
+  #app = mkBinary;
+
+    installPhase = ''
+      mkdir -p $out/bin $out/lib $out/obj $out/tmp
+      echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
+      cp -r . $out/tmp
+      cp -r src/dbb-* $out/bin
+      rm -rf $PWD
+    '';
+
+    postInstall = ''
+      wrapProgram "$out/bin/dbb-cli" --prefix PATH : "$out/tmp"
+      wrapProgram "$out/bin/dbb-app" --prefix PATH : "$out/tmp"
+    '';
   };
-
-  version = "2.2.2";
-
-  buildInputs = [
-    curl
-    libevent
-    libtool
-    libudev
-    libusb
-    libqrencode
-    pkgconfig
-  ] ++ gccPkgs ++ utilPkgs ++ qtPkgs;
-
-  qtPkgs = [
-    qt56.full
-  ];
-
-  gccPkgs = [
-    autoconf
-    automake
-    gcc49
-  ];
-
-  utilPkgs = [
-    #file
-    git
-    #less
-    #strace
-    tree
-    #which
-  ];
-
-  QTDIR="${qt56.qtbase.dev}";
-
-  MOC="${qt56.qtbase.dev}/bin/moc";
-  UIC="${qt56.qtbase.dev}/bin/uic";
-  RCC="${qt56.qtbase.dev}/bin/rcc";
-  LRELEASE="${qt56.qttools.dev}/bin/lrelease";
-  LUPDATE="${qt56.qttools.dev}/bin/lupdate";
-
-  preConfigure = "./autogen.sh";
-
-  configureFlags = [
-    "--enable-debug"
-    "--enable-libusb"
-    #"--with-qt-bindir=${qt56.qtbase.dev}/bin:${qt56.qttools.dev}/bin"
-    #"--with-qt-bindir=${QT_BINDDIR}"
-  ];
-
-  hardeningDisable = [
-    "all"
-    #"format"
-  ];
-
-  fhsEnv = buildFHSUserEnv {
-    inherit name;
-  };
-
-  installPhase = ''
-    mkdir -p $out/bin
-    tree -L 2
-    cp src/dbb-* "$out/bin/"
-      ${stdenv.lib.optionalString withGui ''
-    ''}
-  '';
-}
+in writeScriptBin "dbb-cli" ''
+  #!${stdenv.shell}
+  ${fhsEnv}/bin/dbb-fhs ${base}/bin/dbb-cli
+''
