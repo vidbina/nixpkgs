a497b9ac3d4770228ba017bace9c545ea60fc433
David Asabina <vid@bina.me>
2018-01-10 14:04:58 +0100

    simplified to the point that cli works w/o FHS

--- pkgs/applications/misc/digitalbitbox/default.nix
+++ pkgs/applications/misc/digitalbitbox/default.nix
@@ -3,7 +3,6 @@
   automake,
   buildFHSUserEnv,
   curl,
-  withGui ? true,
   fetchFromGitHub,
   gcc49,
   git,
@@ -21,97 +20,80 @@
 }:
 
 let
-  fhsEnv = buildFHSUserEnv { name = "dbb-fhs"; };
-  base = stdenv.mkDerivation rec {
-    name = "dbb-app";
-
-    src = fetchFromGitHub {
-      owner = "digitalbitbox";
-      repo = "dbb-app";
-      rev = "v${version}";
-      sha256 = "1r77fvqrlaryzij5dfbnigzhvg1d12g96qb2gp8dy3xph1j0k3s1";
-    };
-
-    version = "2.2.2";
-
-    #nativeBuildInputs = gccPkgs;
-
-    buildInputs = [
-      curl
-      libevent
-      libtool
-      libudev
-      libusb
-      libqrencode
-      pkgconfig
-    ] ++ gccPkgs ++ utilPkgs ++ qtPkgs;
-
-    qtPkgs = [
-      qt56.full
-    ];
-
-    gccPkgs = [
-      autoconf
-      automake
-      gcc49
-      #makeWrapper
-    ];
-
-    utilPkgs = [
-      #file
-      git
-      #less
-      #strace
-      tree
-      #which
-    ];
-
-    QTDIR="${qt56.qtbase.dev}";
-
-    MOC="${qt56.qtbase.dev}/bin/moc";
-    UIC="${qt56.qtbase.dev}/bin/uic";
-    RCC="${qt56.qtbase.dev}/bin/rcc";
-    LRELEASE="${qt56.qttools.dev}/bin/lrelease";
-    LUPDATE="${qt56.qttools.dev}/bin/lupdate";
-
-    preConfigure = "./autogen.sh";
-
-    configureFlags = [
-      "--enable-debug"
-      "--enable-libusb"
-      "--with-qt-bindir=${qt56.qtbase.dev}/bin:${qt56.qttools.dev}/bin"
-      #"--prefix=`realpath out`/tmp"
-    ];
-
-    hardeningDisable = [
-      "all"
-      #"format"
-    ];
-
-  #  writeTextFile {
-  #    name = "dbb-cli";
-  #    destination = "/bin/dbb-cli";
-  #    executable = true;
-  #    text = ''
-  #    '';
-  #  };
-
-  #app = mkBinary;
-
-    installPhase = ''
-      mkdir -p $out/bin $out/lib $out/obj $out/tmp
-      echo "AAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
-      cp -r . $out/tmp
-      cp -r src/dbb-* $out/bin
-      rm -rf $PWD
-    '';
-
-    postInstall = ''
-      wrapProgram "$out/bin/dbb-cli" --prefix PATH : "$out/tmp"
-      wrapProgram "$out/bin/dbb-app" --prefix PATH : "$out/tmp"
-    '';
+  reqPkgs = [
+    curl
+    libevent
+    libtool
+    libudev
+    libusb
+    libqrencode
+    pkgconfig
+    qt56.full
+  ];
+  fhsEnv = buildFHSUserEnv {
+    name = "dbb-fhs";
+    targetPkgs = pkgs: reqPkgs;
   };
-in writeScriptBin "dbb-cli" ''
-  #!${stdenv.shell}
-  ${fhsEnv}/bin/dbb-fhs ${base}/bin/dbb-cli
-''
+in
+ stdenv.mkDerivation rec {
+  name = "dbb-app";
+
+  src = fetchFromGitHub {
+    owner = "digitalbitbox";
+    repo = "dbb-app";
+    rev = "v${version}";
+    sha256 = "1r77fvqrlaryzij5dfbnigzhvg1d12g96qb2gp8dy3xph1j0k3s1";
+  };
+
+  version = "2.2.2";
+
+  buildInputs = reqPkgs ++ gccPkgs ++ utilPkgs;
+
+  gccPkgs = [
+    autoconf
+    automake
+    gcc49
+    #makeWrapper
+  ];
+
+  utilPkgs = [
+    #file
+    git
+    #less
+    #strace
+    tree
+    #which
+  ];
+
+  QTDIR="${qt56.qtbase.dev}";
+
+  MOC="${qt56.qtbase.dev}/bin/moc";
+  UIC="${qt56.qtbase.dev}/bin/uic";
+  RCC="${qt56.qtbase.dev}/bin/rcc";
+  LRELEASE="${qt56.qttools.dev}/bin/lrelease";
+  LUPDATE="${qt56.qttools.dev}/bin/lupdate";
+
+  configurePhase = ''
+    ./autogen.sh
+    ./configure --prefix=$out --enable-debug --enable-libusb $configureFlags
+  '';
+
+  hardeningDisable = [
+    "all"
+  ];
+
+  installPhase = ''
+    make install
+    mkdir -p $out/bin $out/lib
+    cp src/libbtc/.libs/*.so* $out/lib
+    cp src/libbtc/src/secp256k1/.libs/*.so* $out/lib
+    cp src/hidapi/libusb/.libs/*.so* $out/lib
+    cp src/univalue/.libs/*.so* $out/lib
+    rm -rf $PWD
+  '';
+
+  postInstall = ''
+    wrapProgram "$out/bin/dbb-cli" --prefix LD_LIBRARY_PATH : "$out/lib"
+    wrapProgram "$out/bin/dbb-app" --prefix LD_LIBRARY_PATH : "$out/lib"
+  '';
+}
